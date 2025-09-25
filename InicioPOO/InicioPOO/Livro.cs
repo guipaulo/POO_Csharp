02c473b38938616dc1decfd91e779e31851bb93a
Paulo Guilherme <pauloguilherme_cn@outlook.com>
2025-09-24 21:05:43 -0300

    Primeiro código POO com Csharp

---
+++ InicioPOO/InicioPOO/Livro.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InicioPOO
+{
+    public class Livro
+    {
+        public string titulo;
+        public string autor;
+        public int paginas;
+        public int paginaAtual;
+
+        public Livro (string titulo, string autor, int paginas)
+        {
+            this.titulo = titulo;
+            this.autor = autor;
+            this.paginas = paginas;
+            paginaAtual = 1;
+        }
+
+        public void LerPagina()
+        {
+            paginaAtual++;
+        }
+
+        public double ObterProgresso()
+        {
+            return (paginaAtual / paginas) * 100;
+        }
+
+        public double ReiniciarLeitura()
+        {
+            return paginaAtual = 1;
+        }
+    }
+}
---
+++ InicioPOO/InicioPOO/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InicioPOO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Livro L1 = new Livro("Harry Potter", "Ellen Mayara", 25);
+
+            L1.LerPagina();
+            L1.LerPagina();
+            L1.LerPagina();
+            L1.LerPagina();
+            L1.LerPagina();
+
+            Console.WriteLine(L1.paginaAtual);
+
+            L1.ReiniciarLeitura();
+            Console.WriteLine(L1.paginaAtual);
+
+            Console.WriteLine(L1.ObterProgresso());
+        }
+    }
+}
