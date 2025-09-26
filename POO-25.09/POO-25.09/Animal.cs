0683b7512575a227c08fc7909326dcd98969be9f
Paulo Guilherme <pauloguilherme_cn@outlook.com>
2025-09-26 20:45:42 -0300

    Exercício de Programação Orientada a Objetos com C#

---
+++ POO-25.09/POO-25.09/Animal.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO_25._09
+{
+    internal class Animal
+    {
+        public string nome;
+        public string especie;
+        public int idade;
+        public bool EstaDormindo;
+
+        public Animal (string nome, string especie, int idade) 
+        { 
+            this.nome = nome;
+            this.especie = especie;
+            this.idade = idade;
+            EstaDormindo = false;
+        }
+
+        public void dormir ()
+        {
+            EstaDormindo = true;
+            Console.WriteLine("O animal está dormindo");
+        }
+
+        public void acordar()
+        {
+            EstaDormindo = false;
+            Console.WriteLine("O animal acordou");
+        }
+
+        public void fazerSom()
+        {
+            Console.WriteLine("O animal fez som!");
+        }
+
+        public void apresentarSe()
+        {
+            Console.WriteLine (this.nome);
+            Console.WriteLine(this.especie);
+            Console.WriteLine(this.idade);
+        }
+    }
+}
---
+++ POO-25.09/POO-25.09/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO_25._09
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal a1 = new Animal("Mimi", "Felino", 3);
+            Animal a2 = new Animal("Rex", "Canino", 3);
+            Animal a3 = new Animal("Piu", "Ave", 1);
+
+            a1.dormir();
+            a2.acordar();
+            a3.fazerSom();
+            a1.apresentarSe();
+        }
+    }
+}
