8dfa711e3fc5a2fd576e05db267cae0d60f73152
Paulo Guilherme <pauloguilherme_cn@outlook.com>
2025-10-20 18:51:10 -0300

    contador seguro

---
+++ contador-seguro/contador-seguro/ContadorSeguro.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace contador_seguro
+{
+    public class ContadorSeguro
+    {
+        private int contador = 0;
+        private int valorMaximo;
+
+        public ContadorSeguro(int valorMax)
+        {
+            this.valorMaximo = valorMax;
+        }
+
+        public void Incrementar()
+        {
+            if (this.contador < valorMaximo)
+            {
+                this.contador++;
+            }
+            else
+            {
+                Console.WriteLine("Valor máximo atingido");
+            }
+        }
+
+        public void Decrementar()
+        {
+            if (this.contador > 0)
+            {
+                this.contador--;
+            }
+            else
+            {
+                Console.WriteLine("Valor mínimo atingido");
+            }
+        }
+
+        public void ObterContador()
+        {
+            Console.WriteLine($"Valor atual do contador: {this.contador} de {valorMaximo}");
+        }
+
+        public void Resetar()
+        {
+            this.contador = 0;
+        }
+    }
+}
---
+++ contador-seguro/contador-seguro/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace contador_seguro
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ContadorSeguro contador1 = new ContadorSeguro(100);
+
+            contador1.Incrementar();
+            contador1.ObterContador();
+
+            contador1.Incrementar();
+            contador1.Incrementar();
+            contador1.Incrementar();
+            contador1.ObterContador();
+
+            contador1.Decrementar();
+            contador1.Decrementar();
+            contador1.ObterContador();
+
+            contador1.Resetar();
+            contador1.ObterContador();
+        }
+    }
+}
