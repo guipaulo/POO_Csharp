9b0ab703f105304f62a6d5c1042b0c150b0d8c2b
Paulo Guilherme <pauloguilherme_cn@outlook.com>
2025-10-20 18:30:20 -0300

    conta-bancaria

---
+++ conta-bancaria/conta-bancaria/ContaBancaria.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace conta_bancaria
+{
+    public class ContaBancaria
+    {
+        public double Saldo {  get; private set; }
+
+        public ContaBancaria(double saldo)
+        {
+            this.Saldo = saldo;
+        }
+
+        public void Depositar(double valor)
+        {
+            this.Saldo += valor;
+        }
+
+        public bool Sacar(double valor)
+        {
+            if (this.Saldo >= valor)
+            {
+                this.Saldo -= valor;
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("Saldo insuficiente!");
+                return false;
+            }
+        }
+
+        public void ExibirSaldo()
+        {
+            Console.WriteLine($"Seu saldo é de {this.Saldo}");
+        }
+    }
+}
---
+++ conta-bancaria/conta-bancaria/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace conta_bancaria
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaBancaria Conta1 = new ContaBancaria(1000);
+
+            Conta1.Sacar(200);
+            Conta1.ExibirSaldo();
+
+            Conta1.Depositar(100);
+            Conta1.ExibirSaldo();
+        }
+    }
+}
