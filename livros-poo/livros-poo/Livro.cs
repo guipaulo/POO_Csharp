206e85496bc1046f8bcad22424983074198fe8e6
Paulo Guilherme <pauloguilherme_cn@outlook.com>
2025-10-20 18:14:28 -0300

    classe livro

---
+++ livros-poo/livros-poo/Livro.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace livros_poo
+{
+    internal class Livro
+    {
+        public string titulo;
+        public string autor;
+        public int paginas;
+        public int paginaAtual = 0;
+
+        public Livro(string titulo, string autor, int paginas)
+        {
+            this.titulo = titulo;
+            this.autor = autor;
+            this.paginas = paginas;
+
+        }
+
+        public void LerPagina()
+        {
+            this.paginaAtual++;
+        }
+
+        public void ObterProgresso()
+        {
+            Console.WriteLine($"Você leu {paginaAtual} páginas de {paginas} páginas");
+        }
+
+        public void ReiniciarLeitura()
+        {
+            paginaAtual = 1;
+        }
+    }
+}
---
+++ livros-poo/livros-poo/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace livros_poo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Livro liv1 = new Livro("A culpa é das estrelas", "John Green", 288);
+            Livro liv2 = new Livro("Harry Potter e a Pedra Filosofal", "J. K. Rowling", 263);
+            Livro liv3 = new Livro("As Crônicas de Nárnia", "C. S. Lewis", 528);
+
+            liv1.LerPagina();
+            liv1.LerPagina();
+            liv1.LerPagina();
+            liv1.LerPagina();
+            liv1.LerPagina();
+
+            liv1.ObterProgresso();
+
+            liv2.LerPagina();
+            liv2.LerPagina();
+            liv2.LerPagina();
+            liv2.LerPagina();
+
+            liv2.ObterProgresso();
+
+            liv3.LerPagina();
+            liv3.LerPagina();
+            liv3.LerPagina();
+
+            liv3.ObterProgresso();
+            liv3.ReiniciarLeitura();
+            liv3.ObterProgresso();
+        }
+    }
+}
